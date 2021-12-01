a5f6fa13e47faba6936b90bc49b4a8a53aaf6b06
Arthur Wouters <arthur@grinjo.nl>
2021-12-01 22:50:21 +0100

    Some extra days and all the input

---
+++ src/AdventOfCode2021/Day03/Day03.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MueR\AdventOfCode\AdventOfCode2021\Day03;
+
+use JetBrains\PhpStorm\Pure;
+use MueR\AdventOfCode\AbstractSolver;
+
+/**
+ * Day 3 puzzle.
+ *
+ * @property array{int} $input
+ */
+class Day03 extends AbstractSolver
+{
+    #[Pure]
+    public function partOne(): int
+    {
+        $result = 0;
+
+        return $result;
+    }
+
+    #[Pure]
+    public function partTwo(): int
+    {
+        $result = 0;
+
+        return $result;
+    }
+}
